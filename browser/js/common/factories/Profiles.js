21b3379be6c7527b48ec07532c535e76404322a9
joanaz <rz258@cornell.edu>
2015-09-25 15:13:21 -0400

    c

--- browser/js/common/factories/Profiles.js
+++ browser/js/common/factories/Profiles.js
@@ -1,27 +1,46 @@
 app.factory('ProfilesFactory', function($http) {
   var companies = [{
-    image: "http://images.vcpost.com/data/images/full/13767/2u-inc.jpg?w=590",
+    image: "/2U.png",
     title: "2U",
-    text: "Helping top-tier universities make leading programs available to more deserving students everywhere.",
-    description: ["Headquarters: Hyattsville, MD", "Founders: John Katzman, Jeremy Johnson, Chip Paucek", "Founded: 2008", "Categories: EdTech, Software, Education", "Website: http://2u.com", "2U is a cloud-based Software-as-a-Service platform that provides schools with the comprehensive operating infrastructure."],
+    text: "Increasing access to quality education by partnering with leading universities to offer degree programs online",
+    description: "2U enables leading colleges and universities to deliver their high-quality education to qualified students anywhere. Its cloudbased software-as-a-service platform provides schools with the comprehensive operating infrastructure they need to attract, enroll, educate, support and graduate their students. 2U ensures that every student can experience the highest quality university education for the most successful outcome.",
+    goals: ["Number of graduates", "Affordability and accessibility", "Savings from online education", "Educational achievement", "Success in workplace after completing the degree"],
+    details: ["Headquarters: Hyattsville, MD", "Founders: John Katzman, Jeremy Johnson, Chip Paucek", "Founded: 2008", "Categories: EdTech, Software, Education", "Website: http://2u.com"],
     metrics: [{
+      image: "graph.png",
       number: "10,000+",
       text: "Students",
       date: "Jun 04, 14"
     }, {
-      number: "17",
-      text: "Programs",
+      image: "83percent.png",
+      number: "83%",
+      text: "Students graduated/still enrolled",
       date: "Nov 04, 14"
     }, {
+      image: "graph.png",
       number: "20,000+",
       text: "Field Placements",
       date: "Sep 18, 14"
     }, {
+      image: "92percent.png",
       number: "92%",
       text: "Post-Grad Job Placement Rate",
       date: "Dec 31, 13"
     }],
     stories: [{
+      image: "/student1.png",
+      text: "In a single day, I can fulfill my duties at the Air Force Base, discuss current events in data privacy during my live MIDS sessions, and then walk to the weekly Code for America hack session. I am so thankful MIDS allows me to extend my understanding of data science academically at Berkeley while continuing my endeavors in Boston.",
+      author: "Erin Boehmer, Masters of Information and Data Science UC Berkeley, Systems engineer for the United States Air Force"
+    }, {
+      image: "/student2.png",
+      text: "I’ve got a demanding job, and I lost my father during the first year of the program and my grandfather six months later… Without question, at these moments it was the most supportive and cooperative program I could imagine being a part of. The flexible nature of the program also allows me to be an active part of the UNC community wherever I am. I’ve gone to class in airports…in my car. You can’t do that with a program that’s traditional brick and mortar.",
+      author: "Candice Brown, Masters of Business Administration UNC Kenan-Flagler, UNC Kenan-Flagler Alumni Council"
+    }, {
+      image: "/Jackson.jpg",
+      text: "At the end of my first immersion in September 2014, I saw many of my fellow students in tears — they were sad to conclude the great experience we had during those few days. I have never experienced anything like that in an academic setting.",
+      author: "Thomas Jackson, MHA@GW student"
+    }],
+    news: [{
       url: "http://www.prnewswire.com/news-releases/2u-inc-announces-pricing-of-public-offering-of-common-stock-300149020.html",
       image: "http://content.prnewswire.com/designimages/logo-prn.gif",
       title: "2U, Inc. Announces Pricing of Public Offering of Common Stock ",
