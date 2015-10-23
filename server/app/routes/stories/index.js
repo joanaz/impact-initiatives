608917ab4b977d3582b1698fada1679115d2e18c
root <root@ip-172-31-31-199.us-west-2.compute.internal>
2015-10-23 00:39:17 +0000

    Add index route for stories

--- server/app/routes/stories/index.js
+++ server/app/routes/stories/index.js
@@ -2,3 +2,15 @@
 var router = require('express').Router();
 module.exports = router;
 var _ = require('lodash');
+
+var mongoose = require('mongoose');
+var Story = mongoose.model('Story');
+
+// get all stories (optionally sort by parameters)
+router.get('/', function(req, res, next) {
+    Story.find({}).exec()
+    .then(function(stories) {
+        res.json(stories);
+    })
+    .then(null, next);
+});
