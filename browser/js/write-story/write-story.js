3fdd8ac71b917461d16328e11bf6fe0f72caee7e
joanaz <rz258@cornell.edu>
2015-10-31 18:14:15 -0400

    c

--- browser/js/write-story/write-story.js
+++ browser/js/write-story/write-story.js
@@ -7,59 +7,16 @@ app.config(function($stateProvider) {
 
 
       $scope.changeState = function() {
-        var file = $scope.myFile;
-        console.log('file is ');
-        console.dir(file);
-        var uploadUrl = "";
-        fileUpload.uploadFileToUrl(file, uploadUrl);
 
 
         $scope.company.stories.unshift({
           author: "Joanna Zhang",
           profile: "http://s3.amazonaws.com/37assets/svn/765-default-avatar.png",
-          date: "23-10-2015",
-          text: $scope.newStory
+          date: new Date(),
+          rating: $scope.rating,
+          text: $scope.newStory,
+          image: ""
         })
-        $state.go('page2', {
-          url: $scope.company.url
-        })
-      }
-    }
-  })
-})
-
-app.config(function($stateProvider) {
-  $stateProvider.state('page2', {
-    url: '/companies/:url/write/2',
-    templateUrl: 'js/write-story/page2.html',
-    controller: ($scope, $state, $stateParams, ProfilesFactory) => {
-      $scope.company = ProfilesFactory.getCompany($stateParams.url)
-      $scope.changeState = function() {
-        $state.go('page3', {
-          url: $scope.company.url
-        })
-      }
-    }
-  })
-})
-
-app.config(function($stateProvider) {
-  $stateProvider.state('page3', {
-    url: '/companies/:url/write/3',
-    templateUrl: 'js/write-story/page3.html',
-    controller: ($scope, $state, $stateParams, ProfilesFactory) => {
-      $scope.company = ProfilesFactory.getCompany($stateParams.url)
-      $scope.photos = []
-
-      for (var i = 0, length = $scope.company.stories.length; i < length; i++) {
-
-        if ($scope.company.stories[i].image) {
-          $scope.photos.push($scope.company.stories[i].image)
-          if ($scope.photos.length >= 2) break
-        }
-      }
-
-      $scope.changeState = function() {
         $state.go('page4', {
           url: $scope.company.url
         })
@@ -67,6 +24,8 @@ app.config(function($stateProvider) {
     }
   })
 })
+
+
 app.config(function($stateProvider) {
   $stateProvider.state('page4', {
     url: '/companies/:url/write/4',
